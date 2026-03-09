oe-alliance/AutoBouquetsMaker

commit 759216d8617efae9615b3b9999947f38eacefcc4

﻿Make "custom" a provider setting, not a bouquet key

AutoBouquetsMaker/src/scanner/providerconfig.py
@@ -73,55 +73,64 @@ class ProviderConfig():
 		return (self.flags & 0x20) == 0x20
 
 	def setMakeNormalMain(self):
-		self.flags = (self.flags & 0xbc) | 0x01
+		self.flags = (self.flags & 0x1bc) | 0x01
 
 	def setMakeCustomMain(self):
-		self.flags = (self.flags & 0xbc) | 0x02
+		self.flags = (self.flags & 0x1bc) | 0x02
 
 	def setMakeHDMain(self):
-		self.flags = (self.flags & 0xbc) | 0x03
+		self.flags = (self.flags & 0x1bc) | 0x03
 
 	def setMakeFTAHDMain(self):
-		self.flags = (self.flags & 0xbc) | 0x40
+		self.flags = (self.flags & 0x1bc) | 0x40
 
 	def unsetMakeMain(self):
-		self.flags &= 0xbc
+		self.flags &= 0x1bc
 
 	def unsetMakeFTAMain(self):
-		self.flags &= 0xbc
+		self.flags &= 0x1bc
 
 	def unsetMakeFTAHDMain(self):
-		self.flags &= 0xbc
+		self.flags &= 0x1bc
 
 	def setMakeSections(self):
 		self.flags |= 0x04
 
 	def unsetMakeSections(self):
-		self.flags &= 0xfb
+		self.flags &= 0x1fb
 
 	def setMakeHD(self):
 		self.flags |= 0x08
 
 	def unsetMakeHD(self):
-		self.flags &= 0xf7
+		self.flags &= 0x1f7
 
 	def setMakeFTA(self):
 		self.flags |= 0x10
 
 	def unsetMakeFTA(self):
-		self.flags &= 0xef
+		self.flags &= 0x1ef
 
 	def setMakeFTAHD(self):
 		self.flags |= 0x80
 
 	def unsetMakeFTAHD(self):
-		self.flags &= 0x7f
+		self.flags &= 0x17f
 
 	def setSwapChannels(self):
 		self.flags |= 0x20
 
 	def unsetSwapChannels(self):
-		self.flags &= 0xdf
+		self.flags &= 0x1df
+
+	def setCustomList(self):
+		self.flags |= 0x100
+
+	def isCustomList(self):
+		return (self.flags & 0x100) == 0x100
+
+	def unsetCustomList(self):
+		self.flags &= 0xff
 
 	def unsetAllFlags(self):
 		self.flags = 0x00

AutoBouquetsMaker/CONTROL/control
@@ -1,5 +1,5 @@
 Package: enigma2-plugin-systemplugins-autobouquetsmaker
-Version: 3.4.r0
+Version: 3.5.r0
 Description: Automatically build and update bouquets from the satellite stream.
 Maintainer: Skaman & AndyBlac
 Depends: enigma2

AutoBouquetsMaker/providers/sat_282_sky_irl.xml
@@ -2,6 +2,9 @@
 	<name>Sky ROI</name>
 	<streamtype>dvbs</streamtype>
 	<protocol>sky</protocol>
+	<options 
+		custom_list="yes"
+	/>
 	<transponder
 		orbital_position="282"
 		frequency="11778000"
@@ -17,10 +20,8 @@
 		onid="2"
 	/>
 	<dvbsconfigs>
-		<configuration key="hd_republic_of_ireland" bouquet="0x1008" region="0x32">ROI HD</configuration>
-		<configuration key="hdcustom_republic_of_ireland" bouquet="0x1008" region="0x32">ROI HD Custom</configuration>
-		<configuration key="sd_republic_of_ireland" bouquet="0x1004" region="0x32">ROI SD</configuration>
-		<configuration key="sdcustom_republic_of_ireland" bouquet="0x1004" region="0x32">ROI SD Custom</configuration>
+		<configuration key="hd_republic_of_ireland" bouquet="0x1008" region="0x32">ROI</configuration>
+		<!-- configuration key="sd_republic_of_ireland" bouquet="0x1004" region="0x32">ROI SD</configuration -->
 	</dvbsconfigs>
 	<sections>
 		<section number="101">Entertainment</section>
@@ -115,7 +116,7 @@ try:
 except:
 	is_assigned = True
 
-	custom_mode = True if "custom" in bouquet_key else False
+	custom_mode = provider_config.isCustomList()
 
 	dxNoSDT = 0x1 # details of lamedb flags are in README.txt
 	dxDontshow = 0x2 # details of lamedb flags are in README.txt

AutoBouquetsMaker/src/scanner/dvbscanner.py
@@ -553,7 +553,7 @@ class DvbScanner():
 
 		return logical_channel_number_dict, TSID_ONID_list
 
-	def updateAndReadServicesLCN(self, transponders, servicehacks, TSID_ONID_list, logical_channel_number_dict, service_dict_tmp, protocol, bouquet_key):
+	def updateAndReadServicesLCN(self, transponders, servicehacks, TSID_ONID_list, logical_channel_number_dict, service_dict_tmp, protocol, bouquet_key, provider_config):
 		print("[ABM-DvbScanner] Reading services (LCN)...", file=log)
 
 		if self.sdt_other_table_id == 0x00:
@@ -727,7 +727,7 @@ class DvbScanner():
 			"radio": radio_services
 		}
 
-	def updateAndReadServicesFastscan(self, transponders, servicehacks, logical_channel_number_dict):
+	def updateAndReadServicesFastscan(self, transponders, servicehacks, logical_channel_number_dict, provider_config):
 		print("[ABM-DvbScanner] Reading services (fastscan)...", file=log)
 
 		fd = dvbreader.open(self.demuxer_device, self.fastscan_pid, self.fastscan_table_id, 0xff, self.frontend)
@@ -876,7 +876,7 @@ class DvbScanner():
 			"radio": radio_services
 		}
 
-	def updateAndReadServicesSKY(self, bouquet_id, region_id, bouquet_key, transponders, servicehacks):
+	def updateAndReadServicesSKY(self, bouquet_id, region_id, bouquet_key, transponders, servicehacks, provider_config):
 		print("[ABM-DvbScanner] Reading services (SKY)...", file=log)
 
 		fd = dvbreader.open(self.demuxer_device, self.bat_pid, self.bat_table_id, 0xff, self.frontend)
@@ -1119,7 +1119,7 @@ class DvbScanner():
 			"radio": radio_services
 		}
 
-	def updateAndReadServicesFreeSat(self, bouquet_id, region_id, bouquet_key, transponders, servicehacks):
+	def updateAndReadServicesFreeSat(self, bouquet_id, region_id, bouquet_key, transponders, servicehacks, provider_config):
 		print("[ABM-DvbScanner] Reading services (Freesat)...", file=log)
 
 		fd = dvbreader.open(self.demuxer_device, self.bat_pid, self.bat_table_id, 0xff, self.frontend)

AutoBouquetsMaker/src/scanner/manager.py
@@ -222,7 +222,7 @@ class Manager():
 						tmp["logical_channel_number_dict"], tmp["TSID_ONID_list"] = scanner.readLCNBAT(bouquet_id, providers[provider_key]["bouquets"][bouquet_key]["region"], tmp["TSID_ONID_list"])
 					self.services[provider_key] = scanner.updateAndReadServicesLCN(
 						self.transponders, providers[provider_key]["servicehacks"], tmp["TSID_ONID_list"],
-						tmp["logical_channel_number_dict"], tmp["service_dict_tmp"], providers[provider_key]["protocol"], bouquet_key)
+						tmp["logical_channel_number_dict"], tmp["service_dict_tmp"], providers[provider_key]["protocol"], bouquet_key, provider_config)
 
 					ret = len(list(self.services[provider_key]["video"].keys())) > 0 or len(list(self.services[provider_key]["radio"].keys())) > 0
 
@@ -236,7 +236,7 @@ class Manager():
 					tmp = scanner.updateTransponders(self.transponders, True)
 					self.services[provider_key] = scanner.updateAndReadServicesFastscan(
 						self.transponders, providers[provider_key]["servicehacks"],
-						tmp["logical_channel_number_dict"])
+						tmp["logical_channel_number_dict"], provider_config)
 
 					ret = len(list(self.services[provider_key]["video"].keys())) > 0 or len(list(self.services[provider_key]["radio"].keys())) > 0
 
@@ -254,7 +254,7 @@ class Manager():
 					bouquet = providers[provider_key]["bouquets"][bouquet_key]
 					self.services[provider_key] = scanner.updateAndReadServicesSKY(bouquet["bouquet"],
 						bouquet["region"], bouquet["key"], self.transponders,
-						providers[provider_key]["servicehacks"])
+						providers[provider_key]["servicehacks"], provider_config)
 
 					ret = len(list(self.services[provider_key]["video"].keys())) > 0 or len(list(self.services[provider_key]["radio"].keys())) > 0
 
@@ -272,7 +272,7 @@ class Manager():
 					bouquet = providers[provider_key]["bouquets"][bouquet_key]
 					self.services[provider_key] = scanner.updateAndReadServicesFreeSat(bouquet["bouquet"],
 						bouquet["region"], bouquet["key"], self.transponders,
-						providers[provider_key]["servicehacks"])
+						providers[provider_key]["servicehacks"], provider_config)
 
 					ret = len(list(self.services[provider_key]["video"].keys())) > 0 or len(list(self.services[provider_key]["radio"].keys())) > 0
 

AutoBouquetsMaker/src/scanner/providers.py
@@ -98,6 +98,7 @@ class Providers():
 			provider["dependent"] = ''
 			provider["bouquets"] = {}
 			provider["ignore_visible_service_flag"] = 0
+			provider["custom_list"] = False
 			if dom.documentElement.nodeType == dom.documentElement.ELEMENT_NODE and dom.documentElement.tagName == "provider":
 				for node in dom.documentElement.childNodes:
 					if node.nodeType != node.ELEMENT_NODE:
@@ -107,6 +108,10 @@ class Providers():
 						node.normalize()
 						if len(node.childNodes) == 1 and node.childNodes[0].nodeType == node.TEXT_NODE:
 							provider["name"] = self.encodeNODE(node.childNodes[0].data)
+					elif node.tagName == "options":
+						for i in list(range(0, node.attributes.length)):
+							if node.attributes.item(i).name == "custom_list":
+								provider["custom_list"] = self.parseBoolean("custom_list", node.attributes.item(i).value)
 					elif node.tagName == "streamtype":
 						node.normalize()
 						if len(node.childNodes) == 1 and node.childNodes[0].nodeType == node.TEXT_NODE:
@@ -420,3 +425,6 @@ class Providers():
 		except:
 			pass
 		return providers
+
+	def parseBoolean(self, attribute, value):
+		return value.lower() in ("1", attribute, "enabled", "on", "true", "yes")

AutoBouquetsMaker/src/setup.py
@@ -74,6 +74,7 @@ class AutoBouquetsMaker_ProvidersSetup(ConfigListScreen, Screen):
 		self.providers_makefta = {}
 		self.providers_makeftahd = {}
 		self.providers_FTA_only = {}
+		self.providers_custom_list = {}
 		self.providers_order = []
 		self.orbital_supported = []
 
@@ -158,6 +159,7 @@ class AutoBouquetsMaker_ProvidersSetup(ConfigListScreen, Screen):
 			self.providers_makehd[provider] = None
 			self.providers_makefta[provider] = None
 			self.providers_makeftahd[provider] = None
+			self.providers_custom_list[provider] = None
 
 			if len(list(self.providers[provider]["sections"].keys())) > 1:  # only if there's more than one section
 				sections_default = True
@@ -226,6 +228,9 @@ class AutoBouquetsMaker_ProvidersSetup(ConfigListScreen, Screen):
 				if provider in providers_tmp_configs and providers_tmp_configs[provider].isMakeNormalMain():
 					makemain_default = "yes"
 				self.providers_makemain[provider] = ConfigSelection(default=makemain_default, choices=[("yes", _("yes")), ("no", _("no"))])
+
+			if self.providers[provider]["custom_list"]:
+				self.providers_custom_list[provider] = ConfigYesNo(default=(provider in providers_tmp_configs and providers_tmp_configs[provider].isCustomList()))
 
 			arealist = []
 			bouquets = self.providers[provider]["bouquets"]
@@ -305,6 +310,9 @@ class AutoBouquetsMaker_ProvidersSetup(ConfigListScreen, Screen):
 
 					if ((self.providers_makemain[provider] and self.providers_makemain[provider].value == "yes") or (self.providers_makesections[provider] and self.providers_makesections[provider].value is True)) and len(self.providers[provider]["swapchannels"]) > 0:
 						setupList.append(getConfigListEntry(indent + _("Swap channels"), self.providers_swapchannels[provider], _("This option will swap SD versions of channels with HD versions. (eg BBC One SD with BBC One HD, Channel Four SD with with Channel Four HD)")))
+		
+					if self.providers_custom_list[provider]:
+						setupList.append(getConfigListEntry(indent + _("Custom mode"), self.providers_custom_list[provider], _("Enable custom mode to apply provider-specific channel adjustments such as adding, removing or renaming channels.")))
 
 				providers_enabled.append(provider)
 
@@ -370,6 +378,9 @@ class AutoBouquetsMaker_ProvidersSetup(ConfigListScreen, Screen):
 
 				if self.providers_swapchannels[provider] and self.providers_swapchannels[provider].value:
 					provider_config.setSwapChannels()
+
+				if self.providers_custom_list[provider] and self.providers_custom_list[provider].value:
+					provider_config.setCustomList()
 
 				config_string += provider_config.serialize()
 

commit 6dd527d1a6ebd544c33c3be505ece59354dc2f01

Temporary workaround for fallout from https://github.com/oe-alliance/AutoBouquetsMaker/commit/759216d8617efae9615b3b9999947f38eacefcc4

AutoBouquetsMaker/src/scanner/providerconfig.py
@@ -17,6 +17,10 @@ class ProviderConfig():
 				self.customfilename = str(chunks[3].strip())
 			except:
 				return
+			# temporary workaround for fallout from https://github.com/oe-alliance/AutoBouquetsMaker/commit/759216d8617efae9615b3b9999947f38eacefcc4
+			if self.provider.startswith("sat_282_sky") and "custom" in self.area:
+				self.area = self.area.replace("custom", "")
+				self.setCustomList()
 
 	def isValid(self):
 		return len(self.provider) > 0